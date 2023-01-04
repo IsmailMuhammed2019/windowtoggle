8b48a1e3968f7cba796205a1f2e45104a0301912
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-04 12:43:40 +0100

    add the window tracker file and component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import WindowTracker from './WindowTracker'
 
 function App() {
+  const [show, setShow] =React.useState(true)
+
+  function toggle() {
+    setShow(prev => !prev)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <button>Toggle WindowTracker</button>
+      {show && <windowTracker />}
     </div>
   );
 }
