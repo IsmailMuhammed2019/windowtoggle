08102a3b330c258a56819ebab1fff6a2db4596f0
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-04 15:55:59 +0100

    clean app component to render update windowtracker

--- src/App.js
+++ src/App.js
@@ -3,15 +3,9 @@ import './App.css';
 import WindowTracker from './WindowTracker'
 
 function App() {
-  const [show, setShow] =React.useState(true)
-
-  function toggle() {
-    setShow(prev => !prev)
-  }
   return (
     <div className="App">
-      <button onClick={toggle}>Toggle WindowTracker</button>
-      {show && <WindowTracker />}
+      <WindowTracker />
     </div>
   );
 }
